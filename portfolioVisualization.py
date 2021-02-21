289a9e7bc057df351d5eb3efb217f72dda93aa43
Homero de Paula <42121039+homerodepaula@users.noreply.github.com>
2021-02-20 22:28:01 -0300

    Update portfolioVisualization.py

--- portfolioVisualization.py
+++ portfolioVisualization.py
@@ -6,3 +6,10 @@ figura = px.line(title = 'Evolução do patrimonio')
 for i in dataset.drop(columns = ['Valor Capital', 'taxa de retorno']).columns:
 	figura.add_scatter(x = datas, y = dataset[i], name = i)				#cada uma das acoes
 figura = px.line(x = datas, y = dataset['Valor Capital'], title = 'Evolução do patrimonio')
+
+#é só pra visualizar acao com % que foi alocado para cada ativo
+
+def visualiza_alocacao(solucao):						#visualiza a solução do problema (portAloc)
+	colunas = dataset_original.columns[1:]
+	for i in range(len(solucao)):
+		print(colunas[i], solucao[i] *100)				#pspsps
